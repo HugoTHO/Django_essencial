e46c002bf5da8a7ae777ff95cda8a43fc3587b7f
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-03-07 05:55:43 -0300

    Capt 4: Criando uma API REST (2)

--- scrum/scrum/settings.py
+++ scrum/scrum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +76,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'scrum',
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
