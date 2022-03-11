2725048cf20a9b45ea2edcd673e5ef116d2a1399
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-03-10 22:36:26 -0300

    Capt 5 - Criação do modelo de sessão no backbone.js

---
+++ scrum/board/static/board/js/models.js
@@ -0,0 +1,80 @@
+(function ($, Backbone, _, app) {
+    
+    // Funções auxiliares de CSRF diretamente extraídas da documentação do Django.
+    function csrfSafeMethod(method) {
+        // esses métodos HTTP não exigem proteção contra CSRF
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/i.test(method));
+    }
+
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = $.trim(cookies[i]);
+
+                // Essa string de cookie começa com o nome que queremos?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(
+                        cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    // Define chamadas jQuery ajax para lidar com CSRF
+    $.ajaxPrefilter(function (settings, originalOptions, xhr) {
+        var csrftoken;
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            // Envia o token somente para os URLs relativos de mesma origem.
+            // Envia o token somente se o método justificar proteção contra CSRF.
+            // O valor de CSRFToken obtido anteriormente está sendo usado.
+            csrftoken = getCookie('csrftoken');
+            xhr.setRequestHeader('X-CSRFToken', csrftoken);
+        }
+    });
+
+    var Session = Backbone.Model.extend({
+        defaults: {
+            token: null
+        },
+        initialize: function (options) {
+            this.options = options;
+            $.ajaxPrefilter($.proxy(this._setupAuth, this));
+            this.load();
+        },
+        load: function () {
+            var token = localStorage.apiToken;
+            if (token) {
+                this.set('token', token);
+            }
+        },
+        save: function (token) {
+            this.set('token', token);
+            if (token === null) {
+                localStorage.removeItem('apiToken');
+            } else {
+                localStorage.apiToken = token;
+            }
+        },
+        delete: function () {
+            this.save(null);
+        },
+        authenticated: function () {
+            return this.get('token') !== null;
+        },
+        _setupAuth: function (settings, originalOptions, xhr) {
+            if (this.authenticated()) {
+                xhr.setRequestHeader(
+                    'Authorization',
+                    'Token ' + this.get('token')
+                );
+            }
+        }
+    });
+
+    app.session = new Session();
+
+})(jQuery, Backbone, _, app);
