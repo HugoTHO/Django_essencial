9e2227d61703484cb40300256b654029bd9f86bc
Hugo Oliveira <hugotholiveira@hotmail.com>
2022-03-11 09:41:41 -0300

    Capt 5 - Feito o roteamento para a tela de login

--- scrum/board/static/board/js/router.js
+++ scrum/board/static/board/js/router.js
@@ -12,8 +12,33 @@
             var view = new app.views.HomepageView({el: this.contentElement});
             this.render(view);
         },
+        route: function (route, name, callback) {
+            // Sobrepõe a rota padrão para obrigar o login em cada página
+            var login;
+            callback = callback || this[name];
+            callback = _.wrap(callback, function (original) {
+                var args = _.without(arguments, original);
+                if (app.session.authenticated()) {
+                    original.apply(this, args);
+                } else {
+                    // Mostra a tela de login antes de chamar a view
+                    $(this.contentElement).hide();
+                    // Liga o callback original uma vez que o login tenha sucesso
+                    login = new app.views.LoginView();
+                    $(this.contentElement).after(login.el);
+                    login.on('done', function () {
+                        $(this.contentElement).show();
+                        original.apply(this, args);
+                    }, this);
+                    // Renderiza o formulário de login
+                    login.render();
+                }
+            });
+            return Backbone.Router.prototype.route.apply(this, [route, name, callback]);
+        },
         render: function (view) {
             if (this.current) {
+                this.current.undelegateEvents();
                 this.current.$el = $();
                 this.current.remove();
             }
